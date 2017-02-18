9bb2255a9e6d88ebedf7b4f93681c53f82f3494e
messense <messense@icloud.com>
2017-02-18 13:31:32 +0800

    兼容 0.39

--- ios/RNGeetest.h
+++ ios/RNGeetest.h
@@ -1,4 +1,8 @@
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 #import <GTFramework/GTFramework.h>
 
 @interface RNGeetest : NSObject <RCTBridgeModule, GTManageDelegate>
--- ios/RNGeetest.m
+++ ios/RNGeetest.m
@@ -1,5 +1,10 @@
+#if __has_include(<React/RCTConvert.h>)
 #import <React/RCTConvert.h>
 #import <React/RCTEventDispatcher.h>
+#else
+#import "RCTConvert.h"
+#import "RCTEventDispatcher.h"
+#endif
 
 #import "RNGeetest.h"
 
